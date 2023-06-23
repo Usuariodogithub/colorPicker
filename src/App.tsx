a1351c69dd5b1db601532d4818500ed5ed91350e
Marcio Lipu <marcio.lipu@gmail.com>
2023-06-22 20:22:12 -0400

    minha avaliação de cpw-iv

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{useState } from 'react';
+import styled from 'styled-components';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+const ColorPickerContainer= styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+padding: 20px;
+`;
+  const ColorPreView=styled.div<{color:string}>`
+  width:200px;
+  height:100px;
+  background-color:${({color})=> color};
+  margin-bottom:20px;
+  `;
+  const ColorSelector= styled.input`
+  width:200px;
+  margin-bottom:10px;
+  `;
+  const App: React.FC = () => {
+    const [color, setColor] = useState<string>('rgba(0, 0, 0, 1)');
+  
+    const handleColorChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+      setColor(event.target.value);
+    };
+  
+    return (
+      <ColorPickerContainer>
+        <ColorPreView color={color} />
+
+        <ColorSelector
+          type="color"
+          value={color}
+          onChange={handleColorChange}
+        />
+        <div>{color}</div>
+      </ColorPickerContainer>
+    );
+  };
+  
+  export default App;
